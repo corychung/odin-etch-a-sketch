fe620aedda618b51a929a4b5f406ba97af92dcde
Cory Chung <cochung@ucsd.edu>
2024-08-29 22:08:02 -0700

    Create second draft
    Finish ironing out bugs. Potential future features include adding a
    prompt in-site for New Grid instead of using alert() and also adding
    eraser and color pickers.

--- script.js
+++ script.js
@@ -3,86 +3,55 @@ const resetBtn = document.querySelector(".new-grid-btn");
 const clearBtn = document.querySelector(".reset-grid-btn");
 const rainbowSwitch = document.querySelector(".rainbow-toggle");
 
-let side = 4;
+const DEFAULT_SIZE = 4;
+const DEFAULT_COLOR = "#212f3d";
 
-// generate random color value to be added into CSS
-function randomColorValue() {
-    let color = [];
-    color.push(Math.floor(Math.random()*255));
-    color.push(Math.floor(Math.random()*255));
-    color.push(Math.floor(Math.random()*255));
-    return `background-color: rgb(${color[0]},${color[1]},${color[2]});`
+let side = DEFAULT_SIZE;
+let colorMode = DEFAULT_COLOR;
+
+// Generate random color value to be added into CSS
+function rainbow() {
+    return `rgb(${Math.random()*255|0},${Math.random()*255|0},${Math.random()*255|0})`;
 }
 
-// make gray
-function setGrayMode() {
-    let boxes = document.querySelectorAll(".grid-box");
-    boxes.forEach(e => {
-        e.addEventListener("mouseenter", () => {
-            console.log(e.style.cssText);
-            e.style.cssText = e.style.cssText + "background-color:gray;";
-            e.setAttribute("class","grid-box")
-        })
-    })
-}
-
-// make rainbow
-function setRainbowMode() {
-    let boxes = document.querySelectorAll(".grid-box");
-    boxes.forEach(e => {
-        e.addEventListener("mouseenter", () => {
-            e.setAttribute("class","grid-box")
-            e.style.cssText = e.style.cssText + randomColorValue();
-        })
-    })
-}
-
-// clear grid
-clearBtn.addEventListener("click", () => {
-    document.querySelectorAll(".grid-box").forEach(e => {
-        let boxes = document.querySelectorAll(".grid-box");
-        boxes.forEach(e => {
-            e.style.cssText = e.style.cssText + "background-color:transparent;";
-        })
-    })
-})
-
-// function to create new grid
+// Clears old grid and creates new grid
 function createGrid(number) {
+    container.innerHTML = "";
     for (let i = 0; i < number; i++) {
         const div = document.createElement("div");
-        div.setAttribute("class","grid-box");
+        div.classList.add("grid-box");
         div.style.width = 100 / side + "%";
         container.appendChild(div);
     }
 }
 
-// create new grid
+// Reset (make new grid) button event listener
 resetBtn.addEventListener("click", () => {
-    side = prompt("How many squares in your grid? (x by x)");
+    side = prompt("Please state desired grid size.");
     if (side === null || side === "0" || side === "") {return;}
-    else {
-        side = parseInt(side);
-        if (side > 50) {
-            alert("Too big! Please choose a smaller size.")
-        }
-        else {
-            console.log(side);
-            document.querySelectorAll(".grid-box").forEach(e => e.remove());
-            createGrid(side * side);
-            rainbowSwitch.checked ? setRainbowMode(): setGrayMode();
-        }
+    if (side > 100) {
+        alert("Too big! Please choose a smaller size.");
+        return;
+    }
+    createGrid(side * side);     
+})
+
+// Rainbow toggle event listener
+rainbowSwitch.addEventListener("change", (e) => {
+    colorMode = rainbowSwitch.checked ? "rainbow" : DEFAULT_COLOR
+  });
+
+// Clear button event listener
+clearBtn.addEventListener("click", () => {
+    createGrid(side * side);
+})
+
+// Main mouse-over event listener
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid-box")) {
+        e.target.style.backgroundColor = (colorMode == "rainbow") ? rainbow() : colorMode;
     }
 })
 
-// toggle rainbow mode switch
-rainbowSwitch.addEventListener("change", (e) => {
-    if (rainbowSwitch.checked) {
-        setRainbowMode();           
-    } else {
-        setGrayMode();
-    }
-  });
-
-createGrid(16);
-setGrayMode();
+// Main "loop"
+createGrid(DEFAULT_SIZE * DEFAULT_SIZE);
